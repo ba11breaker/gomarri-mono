9d4da84eb3ea55dfc3beb5bc9f8f2acbf2f3354f
ba11breaker <15212219417@163.com>
2023-03-25 17:23:16 +1000

    feat: add AddRouter

--- framework/trie.go
+++ framework/trie.go
@@ -1,5 +1,10 @@
 package framework
 
+import (
+	"errors"
+	"strings"
+)
+
 // Represent the Tree structure
 type Tree struct {
 	// The root node of the tree
@@ -12,4 +17,134 @@ type node struct {
 	segement string            // The uri string
 	handler  ControllerHandler // The controller handler
 	children []*node           // The children of the node
+}
+
+func newNode() *node {
+	return &node{
+		isLeaf:   false,
+		segement: "",
+		children: []*node{},
+	}
+}
+
+func NewTree() *Tree {
+	return &Tree{
+		root: newNode(),
+	}
+}
+
+// Check if the segment is a wild segment
+// Start with ':'
+func isWildSegment(segment string) bool {
+	return strings.HasPrefix(segment, ":")
+}
+
+// Filter the child nodes by segment rules
+func (n *node) filterChildNodes(segment string) []*node {
+	// If the node's children is empty, return nil
+	if len(n.children) == 0 {
+		return nil
+	}
+
+	// If the segment is a wild segment, return all children
+	if isWildSegment(segment) {
+		return n.children
+	}
+
+	nodes := make([]*node, 0, len(n.children))
+	for _, child := range n.children {
+		// If the child's segment is a wild segment, append it
+		if isWildSegment(child.segement) {
+			nodes = append(nodes, child)
+			//
+		} else if child.segement == segment {
+			nodes = append(nodes, child)
+		}
+	}
+
+	return nodes
+}
+
+func (n *node) matchNode(uri string) *node {
+
+	segments := strings.SplitN(uri, "/", 2)
+
+	segment := segments[0]
+
+	// If the segment is not a wild segment, convert it to uppercase
+	if !isWildSegment(segment) {
+		segment = strings.ToUpper(segment)
+	}
+
+	// Find the children nodes by segment rules
+	children := n.filterChildNodes(segment)
+
+	if children == nil || len(children) == 0 {
+		return nil
+	}
+
+	//
+	if len(segments) == 1 {
+		for _, child := range children {
+			if child.isLeaf {
+				return child
+			}
+		}
+		return nil
+	}
+
+	// If there are two segments, find the node recursively
+	for _, child := range children {
+		if node := child.matchNode(segments[1]); node != nil {
+			return node
+		}
+	}
+
+	return nil
+}
+
+func (tree *Tree) AddRouter(uri string, handler ControllerHandler) error {
+	n := tree.root
+
+	if n.matchNode(uri) != nil {
+		return errors.New("route exist: " + uri)
+	}
+
+	segments := strings.Split(uri, "/")
+
+	for index, segment := range segments {
+		if !isWildSegment(segment) {
+			segment = strings.ToUpper(segment)
+		}
+
+		isLast := index == len(segments)-1
+
+		var objNode *node
+
+		childrenNodes := n.filterChildNodes(segment)
+
+		if len(childrenNodes) == 0 {
+			for _, child := range childrenNodes {
+				if child.segement == segment {
+					objNode = child
+					break
+				}
+			}
+		}
+
+		if objNode == nil {
+			childNode := newNode()
+			childNode.segement = segment
+			if isLast {
+				childNode.isLeaf = true
+				childNode.handler = handler
+			}
+			n.children = append(n.children, childNode)
+			objNode = childNode
+		}
+
+		n = objNode
+	}
+
+	return nil
 }
